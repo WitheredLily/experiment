f4d39902081d66c5c1c9b70cb4651673a947e28e
Ted Clarke <40334383@ads.qub.ac.uk>
2025-10-24 17:03:56 +0100

    Stuff

--- lib/ghast-api-stack.ts
+++ lib/ghast-api-stack.ts
@@ -1,32 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as apigw from "aws-cdk-lib/aws-apigateway";
-import * as lambda from "aws-cdk-lib/aws-lambda-nodejs";
-import path from "path";
-import {Runtime} from "aws-cdk-lib/aws-lambda";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import * as path from 'path';
+import * as lambda from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Runtime } from "aws-cdk-lib/aws-lambda";
 
-export class StuffStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'StuffQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-      const api = new apigw.RestApi(this, `stuffApiGateway`, {
-          restApiName: `ghast-api`,
-          deployOptions: {
-              metricsEnabled: true,
-              loggingLevel: apigw.MethodLoggingLevel.INFO,
-              dataTraceEnabled: true,
-          },
-          cloudWatchRole: true,
-      });
-  }
-}
 
 export class GhastApiStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
